3d44685d2a005de2920b8c31a416dfe7e6a601ba
AndresBW1807 <barajaswellmanportafolio@gmail.com>
2023-03-21 16:51:39 -0500

    Frontend Login

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+
+import {FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+// --------------------PrimeNg----------------------------
+import { AutoCompleteModule } from "primeng/autocomplete";
+import { InputTextareaModule } from "primeng/inputtextarea";
+import { InputTextModule } from "primeng/inputtext";
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    AutoCompleteModule,
+    InputTextModule,
+    InputTextareaModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
